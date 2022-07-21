6cfd9872d400cb5ab36fd059c7a0b8acbc3c8d23
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2022-07-21 21:45:46 +0200

    add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,6 +8,7 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
   game!: Game;
 
 
@@ -24,6 +25,17 @@ export class GameComponent implements OnInit {
   }
 
   takeCard(){
-    this.pickCardAnimation =true;
+    if(!this.pickCardAnimation){
+      this.currentCard = String(this.game.stack.pop()); //Removes the last element from an array and returns it.
+      
+      this.pickCardAnimation =true;
+      console.log('New Card' + this.currentCard);
+      console.log('Game is' , this.game);
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,6 +1,37 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Hans', 'Freddy', 'Peter'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
+
+    constructor(){
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('spade_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+
+        shuffle(this.stack);
+    }
 }
+
+
+// from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array: string[]) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
